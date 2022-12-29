13ba5420c563bca2434d91ccbd1102534c8e9bdb
borisdavid77 <borisdavid779@gmail.com>
2022-12-29 10:29:06 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import World from './components/World'
+//import styles from './styles/styles.css'
+
+function App() {
+  return (
+    <>     
+      <World></World>
+    </>
+  )
+}
+
+export default App
